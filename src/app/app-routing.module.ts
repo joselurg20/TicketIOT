6b4122b9724802f4166218c717743fd07f234ff4
Jose_Luis <joselu2004.jlrc@gmail.com>
2024-03-18 12:51:50 +0100

    components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404Component } from './componets/error404/error404.component';
+import { LoginComponent } from './componets/login/login-index/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: LoginComponent},
+  {path: "404", component: Error404Component},
+  {path: "**", redirectTo: "/404"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/componets/grafics/chart-doughnut/chart-doughnut.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-chart-doughnut',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './chart-doughnut.component.html',
+  styleUrls: ['./chart-doughnut.component.scss']
+})
+export class ChartDoughnutComponent {
+
+}

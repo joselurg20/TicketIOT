f5114d0d9e7d21cba53e20fa5ca802517c27fc4e
Jose_Luis <joselu2004.jlrc@gmail.com>
2024-03-18 15:11:30 +0100

    cambios del diseño

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PagesComponent
   ],
   imports: [
     BrowserModule,

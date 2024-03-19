4ec4e7effb1983590c1203e2c09e935d5119c5e6
Jose_Luis <joselu2004.jlrc@gmail.com>
2024-03-19 14:16:07 +0100

    review y recovered

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { Error404Component } from './components/error404/error404.component';
 import { LoginComponent } from './components/login/login-index/login.component';
 import { IncidenceIndexComponent } from './components/incidences/incidence-index/incidence-index.component';
 import { RecoveredComponent } from './components/login/recovered/recovered.component';
+import { IncidencesReviewComponent } from './components/incidences/incidences-review/incidences-review.component';
 
 
 const routes: Routes = [
   {path: "", component: LoginComponent},
   {path: "login", component: LoginComponent},
   {path: "recuperar", component: RecoveredComponent},
+  {path: "revisar", component: IncidencesReviewComponent},
   {path: "incidencia", component:IncidenceIndexComponent },
   {path: "404", component: Error404Component},
   {path: "**", redirectTo: "/404"}

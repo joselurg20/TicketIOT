0610759cb4c2c4749b6b106e471d4a004fe1a870
Jose_Luis <joselu2004.jlrc@gmail.com>
2024-04-03 14:07:49 +0200

    reset field

--- src/app/components/grafics/chart-pie/chart-pie.component.ts
+++ src/app/components/grafics/chart-pie/chart-pie.component.ts
@@ -86,12 +86,12 @@ export class ChartPieComponent {
           label: 'Cantidad de incidencias',
           data: incidentCounts,
           backgroundColor: [
-            'rgba(232, 19, 87, 0.2)',
-            'rgba(116, 92, 216, 0.2)',
-            'rgba(253, 183, 63, 0.2)',
-            'rgba(59, 235, 151, 0.2)',
-            'rgba(59, 214, 235, 0.2)',
-            'rgba(255, 255, 255, 0.2)'
+            'rgba(232, 19, 87, 1)',
+            'rgba(116, 92, 216, 1)',
+            'rgba(253, 183, 63, 1)',
+            'rgba(59, 235, 151, 1)',
+            'rgba(59, 214, 235, 1)',
+            'rgba(255, 255, 255, 1)'
           ],
           borderColor: [
             'rgba(232, 19, 87, 1)',

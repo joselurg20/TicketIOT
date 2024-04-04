bccc05a9248d785a231d5534aad3f8c09f9df6d9
Jose_Luis <joselu2004.jlrc@gmail.com>
2024-04-04 14:41:02 +0200

    botones puestos

--- src/app/components/login/recovered/recovered.component.ts
+++ src/app/components/login/recovered/recovered.component.ts
@@ -1,12 +1,13 @@
 import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent } from "../../button/button.component";
 
 @Component({
-  selector: 'app-recovered',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './recovered.component.html',
-  styleUrls: ['./recovered.component.scss']
+    selector: 'app-recovered',
+    standalone: true,
+    templateUrl: './recovered.component.html',
+    styleUrls: ['./recovered.component.scss'],
+    imports: [CommonModule, ButtonComponent]
 })
 export class RecoveredComponent {
 }
--- src/app/pages/technicial-incidence/technicial-incidence.component.ts
+++ src/app/pages/technicial-incidence/technicial-incidence.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { IncidenceDataComponent } from "../../components/incidences/incidence-data/incidence-data.component";
 import { ComunicationComponent } from "../../components/messages/comunication/comunication.component";
 import { TechnicialComponent } from "../../components/data/technicial/technicial.component";
+import { ButtonComponent } from "../../components/button/button.component";
 
 @Component({
     selector: 'app-technicial-incidence',
     standalone: true,
     templateUrl: './technicial-incidence.component.html',
     styleUrls: ['./technicial-incidence.component.scss'],
-    imports: [CommonModule, IncidenceDataComponent, ComunicationComponent, TechnicialComponent]
+    imports: [CommonModule, IncidenceDataComponent, ComunicationComponent, TechnicialComponent, ButtonComponent]
 })
 export class TechnicialIncidenceComponent {
 

672feff447c2f0d311baa320b0380e245681bbc4
Juanite <juan13_barsa@hotmail.com>
2024-05-31 08:15:06 +0200

    arreglo selector tecnico

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -49,6 +49,7 @@ export class DataComponent implements OnInit {
     setTimeout(() => {
       if (this.usersService.currentUser?.role === Roles.managerRole) {
         this.isSupportManager = true;
+        this.userDataService.getTechnicians();
         this.userDataService.usersFN$.subscribe({
           next: (response: iUserGraph[]) => {
             const users = response.map((value: iUserGraph) => ({
@@ -57,20 +58,6 @@ export class DataComponent implements OnInit {
               fullName: value.fullName
             }));
             this.users = users;
-
-            // Cargar valores desde localStorage
-            const storedUserId = localStorage.getItem(LocalStorageKeys.selectedUserId);
-            if (storedUserId) {
-              this.selectedUserId = parseInt(storedUserId);
-            }
-            const storedPriorityValue = localStorage.getItem(LocalStorageKeys.selectedPriorityValue);
-            if (storedPriorityValue) {
-              this.selectedPriorityValue = parseInt(storedPriorityValue);
-            }
-            const storedStatusValue = localStorage.getItem(LocalStorageKeys.selectedStatusValue);
-            if (storedStatusValue) {
-              this.selectedStatusValue = parseInt(storedStatusValue);
-            }
           },
           error: (error: any) => {
             console.error('Error al obtener los usuarios', error);
@@ -89,7 +76,6 @@ export class DataComponent implements OnInit {
   assignTicket() {
     if (this.selectedUserId != -1) {
       this.loadingService.showLoading();
-      localStorage.setItem(LocalStorageKeys.selectedUserId, this.selectedUserId.toString()); // Guardar en localStorage
       this.ticketsService.assignTechnician(parseInt(localStorage.getItem(LocalStorageKeys.selectedTicket)!), this.selectedUserId).subscribe({
         next: () => {
           this.ticketUpdateService.triggerTicketUpdate();
@@ -109,7 +95,6 @@ export class DataComponent implements OnInit {
   updatePrio() {
     if (this.selectedPriorityValue != -1) {
       this.loadingService.showLoading();
-      localStorage.setItem(LocalStorageKeys.selectedPriorityValue, this.selectedPriorityValue.toString()); // Guardar en localStorage
       this.ticketsService.changeTicketPriority(parseInt(localStorage.getItem(LocalStorageKeys.selectedTicket)!), this.selectedPriorityValue).subscribe({
         next: () => {
           this.ticketUpdateService.triggerTicketUpdate();
@@ -128,7 +113,6 @@ export class DataComponent implements OnInit {
   updateStatus() {
     if (this.selectedStatusValue != -1) {
       this.loadingService.showLoading();
-      localStorage.setItem(LocalStorageKeys.selectedStatusValue, this.selectedStatusValue.toString()); // Guardar en localStorage
       this.ticketsService.changeTicketStatus(parseInt(localStorage.getItem(LocalStorageKeys.selectedTicket)!), this.selectedStatusValue).subscribe({
         next: () => {
           this.ticketUpdateService.triggerTicketUpdate();

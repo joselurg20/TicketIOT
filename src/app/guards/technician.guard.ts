af53d07d0f2756db1bb9f2b5988efeb509a8cd5e
Juanite <juan13_barsa@hotmail.com>
2024-06-05 12:14:42 +0200

    timeouts quitados

--- src/app/guards/technician.guard.ts
+++ src/app/guards/technician.guard.ts
@@ -4,23 +4,25 @@ import { inject } from '@angular/core';
 import { Roles } from '../utilities/literals';
 import { LoginService } from '../services/users/login.service';
 import { Routes } from '../utilities/routes';
+import { ComponentLoadService } from '../services/componentLoad.service';
 
 export const technicianGuard: CanActivateFn = (route, state) => {
   const loginService = inject(LoginService)
   const usersService = inject(UsersService);
   const router = inject(Router);
+  const trigger = inject(ComponentLoadService);
 
   let response = true;
 
   if(loginService.isLogged()){
 
-    setTimeout(() => {
+    trigger.loadComponent$.subscribe(() => {
      
-    if (usersService.currentUser?.role !== Roles.technicianRole) {
-      response = false;
-      router.navigate([Routes.login]);
-    } 
-    },10)
+      if (usersService.currentUser?.role !== Roles.technicianRole) {
+        response = false;
+        router.navigate([Routes.login]);
+      }
+    });
     
   }else{
     response = false;
